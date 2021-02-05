9d3f3d513f3b65f6e92ff3e695fda27bb166639e
vitorsch <vitorsch@gmail.com>
2021-02-05 01:30:22 -0300

    PF-FrontEnd-05-02-2021

--- exercicios-js/projetoFinal/versao-inicial/backend/schedule/statsSchedule.js
+++ exercicios-js/projetoFinal/versao-inicial/backend/schedule/statsSchedule.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule')
 
 module.exports = app => {
     schedule.scheduleJob('*/1 * * * *', async function() {
-        const usersCount = await app.db('users').count('id').first()
+        const usersCount = await app.db('users').whereNull('deletedAt').count('id').first()
         const categoriesCount = await app.db('categories').count('id').first()
         const articlesCount = await app.db('articles').count('id').first()
 
--- exercicios-js/projetoFinal/versao-inicial/frontend/src/components/template/Menu.vue
+++ exercicios-js/projetoFinal/versao-inicial/frontend/src/components/template/Menu.vue
@@ -1,13 +1,43 @@
 <template>
-    <aside class="menu" v-show="isMenuVisible"></aside>
+    <aside class="menu" v-show="isMenuVisible">
+        <div class="menu-filter">
+            <i class="fa fa-search fa-lg"></i>
+            <input type="text" placeholder="Digite para filtrar..."
+                v-model="treeFilter" class="filter-field">
+        </div>
+        <Tree :data="treeData" :options="treeOptions"
+            :filter="treeFilter" ref="tree"/>
+    </aside>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import Tree from 'liquor-tree'
+import { baseApiUrl } from '@/global'
+import axios from 'axios'
 
 export default {
     name: "Menu",
-    computed: mapState(['isMenuVisible'])
+    components: { Tree },
+    computed: mapState(['isMenuVisible']),
+    data: function() {
+        return {
+            treeFilter: '',
+            treeData: this.getTreeData(),
+            treeOptions: {
+                propertyNames: { 'text': 'name' },
+                filter: {
+                    emptyText: 'Categoria não encontrada'
+                }
+            }
+        }
+    },
+    methods: {
+        getTreeData() {
+            const url = `${baseApiUrl}/categories/tree`
+            return axios.get(url).then(res => res.data)
+        }
+    }
 }
 </script>
 
@@ -19,5 +49,49 @@ export default {
         display: flex;
         flex-direction: column;
         flex-wrap: wrap;
+    }
+
+    .menu a,
+    .menu a:hover {
+        color: #fff;
+        text-decoration: none;
+    }
+
+    .menu .tree-node.selected > .tree-content,
+    .menu .tree-node .tree-content:hover {
+        background-color: rgba(255, 255, 255, 0.2);
+    }
+
+    .tree-arrow.has-child {
+        filter: brightness(3)
+    }
+    
+    .menu .menu-filter {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        margin: 20px;
+        padding-bottom: 8px;
+        border-bottom: 1px solid #AAA;
+
+    }
+    .menu .menu-filter input {
+        background-color: rgba(0, 0, 0, 0);
+        border: none;
+        color:#CCC;
+        width: 100%;
+        outline: 0px;
+    }
+    .menu .menu-filter i {
+        color: #AAA;
+        margin-right: 10px;
+
+    }
+
+    .tree-filter-empty {
+        color: #CCC;
+        margin-left: 20px;
+        font-size: 1.1rem;
     }
 </style>
